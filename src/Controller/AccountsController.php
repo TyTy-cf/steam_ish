30fda724a8844e5684b81817211a6ab29b7be68b
Kevin Tourret <tourret.kevin@gmail.com>
2021-03-01 15:33:02 +0100

    add filters & paginator

--- src/Controller/AccountsController.php
+++ src/Controller/AccountsController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Accounts;
 use App\Form\AccountsType;
+use App\Form\Filter\AccountFilterType;
 use App\Repository\AccountRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Lexik\Bundle\FormFilterBundle\Filter\FilterBuilderUpdaterInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,12 +21,37 @@ class AccountsController extends AbstractController
     /**
      * @Route("/", name="accounts_index", methods={"GET"})
      * @param AccountRepository $accountRepository
+     * @param PaginatorInterface $paginator
+     * @param FilterBuilderUpdaterInterface $builderUpdater
+     * @param Request $request
      * @return Response
      */
-    public function index(AccountRepository $accountRepository): Response
-    {
+    public function index(
+        AccountRepository $accountRepository,
+        PaginatorInterface $paginator,
+        FilterBuilderUpdaterInterface $builderUpdater,
+        Request $request
+    ): Response {
+        $qb = $accountRepository->queryAll();
+
+        $filterForm = $this->createForm(AccountFilterType::class, null, [
+            'method' => 'GET'
+        ]);
+
+        if ($request->query->has($filterForm->getName())) {
+            $filterForm->submit($request->query->get($filterForm->getName()));
+            $builderUpdater->addFilterConditions($filterForm, $qb);
+        }
+
+        $accounts = $paginator->paginate(
+            $qb,
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return $this->render('accounts/index.html.twig', [
-            'accounts' => $accountRepository->findAll(),
+            'accounts' => $accounts,
+            'filters' => $filterForm->createView(),
         ]);
     }
 
