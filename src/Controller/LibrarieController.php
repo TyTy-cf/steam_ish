6d3b0d82ab514544cd0339a38cc554a26e8cc8b8
Karim HADJ-ABDELKADER <karim-hadj@hotmail.fr>
2021-03-02 12:04:38 +0100

    merged

--- src/Controller/LibrarieController.php
+++ src/Controller/LibrarieController.php
@@ -6,6 +6,8 @@ namespace App\Controller;
 
 use App\Entity\Accounts;
 use App\Repository\LibrarieRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,14 +26,17 @@ class LibrarieController extends AbstractController
      * @param Accounts $account
      * @param LibrarieRepository $librarieRepository
      * @return Response
+     * @throws NoResultException
+     * @throws NonUniqueResultException
      */
     public function index(
         Accounts $account,
         LibrarieRepository $librarieRepository
     ) {
         return $this->render('librarie_index.html.twig', [
-           'account' => $account,
-           'libraries' => $librarieRepository->findLibrariesByAccount($account),
+            'account' => $account,
+            'libraries' => $librarieRepository->findLibrariesByAccount($account),
+            'totalGameTime' => $librarieRepository->getTotalGameTimeByAccount($account),
         ]);
     }
 
